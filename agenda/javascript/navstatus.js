d9d710cf5cf1712d27218f29155f06b4ef9f6828
Roan Hassan <roan-hassan@live.nl>
2025-02-18 15:40:04 +0100

    Update changes before force push

--- agenda/javascript/navstatus.js
+++ agenda/javascript/navstatus.js
@@ -1,12 +1,14 @@
+// navstatus.js
+
 document.addEventListener('DOMContentLoaded', () => {
   const userMenu = document.getElementById('userMenu');
-  if (!userMenu) return; // Als een pagina geen userMenu-div heeft
+  if (!userMenu) return; // geen userMenu op deze pagina
 
   const token = localStorage.getItem('token');
   const email = localStorage.getItem('email');
 
   if (token && email) {
-    userMenu.innerHTML = `<span id="userNameSpan"> Hier uitloggen, ${email}</span>`;
+    userMenu.innerHTML = `<span id="userNameSpan">Hier uitloggen, ${email}</span>`;
     document.getElementById('userNameSpan').addEventListener('click', () => {
       if (confirm('Wil je uitloggen?')) {
         localStorage.removeItem('token');
